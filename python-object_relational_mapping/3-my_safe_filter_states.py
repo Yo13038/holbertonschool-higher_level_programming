51d2debf17e23a71937ae09e32f5f0b506bd0a88
Yo13038 <13038@holbertonstudents.com>
2026-06-08 15:41:32 +0200

    write script that takes in arguments that is safe from MySQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module to list state from a database
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    data_base = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=data_base
+    )
+
+    cursor = db.cursor()
+
+    query ="""SELECT * FROM states
+                WHERE name = %s
+                ORDER BY id ASC;"""
+    
+    cursor.execute(query, (state_name_searched,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
