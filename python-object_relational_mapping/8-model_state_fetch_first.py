56ac757e9de37e246d6f9ebcd8f581f04c02c4a8
Yo13038 <13038@holbertonstudents.com>
2026-06-08 23:13:55 +0200

    change condition format

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,13 +8,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Utilisation du backslash pour couper proprement sous les 79 caractères
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
     )
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
