e3e1e4717d582e2b766b793e4c32a857fcf92e15
Yo13038 <13038@holbertonstudents.com>
2026-06-08 16:26:19 +0200

    script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Module to list state from a database
+Lists all cities of a specific state provided as an argument,
+from the database hbtn_0e_4_usa. Safe from SQL injection.
 """
-
 import sys
 import MySQLdb
 
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     data_base = sys.argv[3]
     state_name = sys.argv[4]
 
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -21,23 +22,23 @@ if __name__ == "__main__":
         db=data_base
     )
 
+
     cursor = db.cursor()
 
     query = (
         "SELECT cities.id, cities.name, states.name "
         "FROM cities "
-        "INNER JOIN states "
-        "ON cities.state_id = states.id "
-        "WHERE states.name = %s"
+        "INNER JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
         "ORDER BY cities.id ASC;"
     )
 
     cursor.execute(query, (state_name,))
 
     rows = cursor.fetchall()
-
     for row in rows:
         print(row)
+
 
     cursor.close()
     db.close()
