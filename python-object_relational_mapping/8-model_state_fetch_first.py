39cab44ab6037fb162d094d837f8a7b96aee7426
Yo13038 <13038@holbertonstudents.com>
2026-06-08 23:04:42 +0200

    add f to print

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that list state object from database
+Prints the first State object from the database hbtn_0e_6_usa.
 """
 import sys
 from model_state import Base, State
@@ -8,13 +8,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
+    # Découpage propre pour respecter la limite des 79 caractères
     connection_url = (
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+        f"mysql+mysqldb://{username}:{password}"
+        f"@localhost:3306/{db_name}"
     )
     engine = create_engine(connection_url, pool_pre_ping=True)
 
@@ -23,9 +24,9 @@ if __name__ == "__main__":
 
     first_state = session.query(State).order_by(State.id).first()
 
-    if first_state is None:
-        print("Nothing")
-    else:
+    if first_state:
         print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
 
     session.close()
