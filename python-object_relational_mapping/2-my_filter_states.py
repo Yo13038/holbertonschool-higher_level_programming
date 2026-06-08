80ec3310902170285349ebefa3a94e1553600d68
Yo13038 <13038@holbertonstudents.com>
2026-06-08 15:22:49 +0200

    add the forgoten keyword binary to the condition where in query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
     query = (
         "SELECT * FROM states "
-        "WHERE name = '{}' "
+        "WHERE BINARY name = '{}' "
         "ORDER BY id ASC;"
     ).format(state_name_searched)
 
