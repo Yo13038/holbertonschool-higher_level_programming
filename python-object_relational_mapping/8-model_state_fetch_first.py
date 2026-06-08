c98ce2cfddc036b73b4825d5069765f5f9d0f7a0
Yo13038 <13038@holbertonstudents.com>
2026-06-08 23:22:00 +0200

    delete the first statement from print

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""
-Prints the first State object from the database hbtn_0e_6_usa.
-"""
+"""Script that prints the first State object from the database."""
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
-    )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    state = session.query(State).order_by(State.id).first()
-
-    if first_state:
-        print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
-
-    session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    with Session(engine) as session:
+        state = session.query(State).order_by(State.id).first()
+        if state is None:
+            print("Nothing")
+        else:
+            print("{}: {}".format(state.id, state.name))
