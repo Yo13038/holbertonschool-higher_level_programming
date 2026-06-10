fe91a6a4ff14edf3f1376d9d010c097c961e8bbc
Yo13038 <13038@holbertonstudents.com>
2026-06-10 14:32:49 +0200

    simple API using http.server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            
+            data_to_serve = {"name": "John", "age": 30, "city": "New York"}
+            json_string = json.dumps(data_to_serve)
+            self.wfile.write(json_string.encode("utf-8"))
+            
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+def run():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, SimpleAPIHandler)
+    print("Serveur lancé sur le port 8000...")
+
+
+if __name__ == "__main__":
+    run()
