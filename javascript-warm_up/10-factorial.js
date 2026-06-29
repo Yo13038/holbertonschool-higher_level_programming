4db4dbcadd24fad106e879726d6a5ad577f69e56
Yo13038 <13038@holbertonstudents.com>
2026-06-29 21:16:23 +0200

    script that computes and prints a factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+
+function factorial (x) {
+
+  if (Number.isNaN(x) || x <= 1) {
+    return 1;
+  }
+  return x * factorial(x - 1);
+}
+
+const number = parseInt(process.argv[2], 10);
+
+console.log(factorial(number));
