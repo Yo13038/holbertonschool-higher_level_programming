3e053f3209ac07a39423252f737beccc7cba69a6
Yo13038 <13038@holbertonstudents.com>
2026-06-29 15:41:13 +0200

    correct desired output

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const myNumber = parseInt(process.argv[2], 10);
 if (Number.isNaN(myNumber)) {
   console.log('Not a number');
 } else {
-  console.log('My number:' + myNumber);
+  console.log('My number: ' + myNumber);
 }
